13db8fcc22090c61162120be289c319190141e4c
Ethan4269875321 <ethansevenster1005@gmail.com>
2023-10-24 07:04:21 -0700

    My space login

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleForm() {
+  const container = document.querySelector(".container");
+  const section = document.querySelector("section");
+  container.classList.toggle("active");
+  section.classList.toggle("active");
+}
